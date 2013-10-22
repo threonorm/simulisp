3471559ab4760e41f54a41fcc75a7f33a08f9ffe
thomas bourgeat <thomas.07fr@gmail.com>
2013-10-22 14:40:16 +0200

    readexp

--- haskell/Scheduler.hs
+++ haskell/Scheduler.hs
@@ -1,30 +1,32 @@
 module Scheduler where
 
 import Prelude hiding (notElem, mapM_)
-import Control.Monade.State hiding (mapM_)
+import Control.Monad.State hiding (mapM_)
 import Control.Applicative
 import Data.Maybe
 import Data.Foldable
-import qualified Data.Mape as Map
+import Data.List (nub) 
+import qualified Data.Map as Map
 import Data.Map (Map, (!))
 
 import NetlistAST 
 
 under_arg arg =
   case arg of 
-    Avar Ident -> [Ident]
-    Aconst Value -> [] 
+    Avar ident -> Just ident
+    Aconst value -> Nothing
 
 read_exp exp = 
   case exp of
-         Earg arg -> under_arg arg 
+         Earg arg -> maybeToList $ under_arg arg 
          Ereg ident -> [ident] 
-         Enot arg-> under_arg arg 
-         Ebinop binop arg1 arg2 -> map under_arg [arg1, arg2, arg3]
-         Emux arg1 arg2 arg3-> map under_arg [arg1, arg2, arg3]   
-         Erom _ _ arg -> under_arg arg
-         Eram _ _ arg1 arg2 arg3 arg4-> map under_arg [arg1, arg2, arg3, arg4] 
-         Econcat arg1 arg2 -> map under_arg [arg1, arg2]
-         Eslice int int arg ->under_arg arg
-         Eselect int arg -> under_arg arg
+         Enot arg-> maybeToList $ under_arg arg 
+         Ebinop binop arg1 arg2 ->nub $ mapMaybe under_arg [arg1, arg2]
+         Emux arg1 arg2 arg3-> nub $ mapMaybe under_arg [arg1, arg2, arg3]   
+         Erom _ _ arg -> maybeToList $ under_arg arg
+         Eram _ _ arg1 arg2 arg3 arg4-> nub $  mapMaybe under_arg [arg1, arg2, arg3, arg4] 
+         Econcat arg1 arg2 -> nub $ mapMaybe under_arg [arg1, arg2]
+         Eslice int1 int2 arg -> maybeToList $ under_arg arg
+         Eselect int arg -> maybeToList $ under_arg arg
+
  
