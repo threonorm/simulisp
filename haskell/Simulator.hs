34b41a0063679738f21e1cf10c2cbdfbeecdea51
thomas bourgeat <thomas.07fr@gmail.com>
2013-11-01 23:12:31 +0100

    Work in progress : Compilation Ok, not tested

--- haskell/Simulator.hs
+++ haskell/Simulator.hs
@@ -3,6 +3,7 @@ module Simulator where
 import Control.Applicative
 import Data.List
 import qualified Data.Map as Map
+import Data.Bool
 
 import NetlistAST
 import Scheduler
@@ -22,9 +23,9 @@ vLogic op v1 v2 =
   case (v1,v2) of
     (VBit a1,VBit a2)->VBit (op a1 a2)
     (VBitArray l1, VBitArray l2)-> VBitArray (map (\(a,b)->op a b) $ zip l1 l2)
-    (VBitArray l1,VBit a1) ->
+    (VBitArray l1,VBit a1) ->  --Intuition : maybe it is not necessary
          VBitArray (map (\(a,b)->op a b) $ zip l1 (repeat a1))
-    (VBit a1,VBitArray l1) -> 
+    (VBit a1,VBitArray l1) ->  --Idem : I'm lazy and don't want read the specs
          VBitArray (map (\(a,b)->op a b) $ zip l1 (repeat a1))
 
 vNot :: Value -> Value
@@ -33,8 +34,37 @@ vNot v= case v of
  (VBitArray l) -> VBitArray (map not $ l) 
 
 
---simulate :: SysState -> Exp -> Value
+simulate :: SysState -> Exp -> Value
+simulate st (Earg a) =
+  extractArg st a
 
+simulate st (Enot a) =
+  vNot . extractArg st $ a
+
+simulate st (Ebinop op a1 a2) =
+  vLogic opTransf vA1 vA2
+  where vA1 = extractArg st a1
+        vA2 = extractArg st a2 
+        opTransf = transfo op
+        transfo x = case x of
+           And  -> (&&) 
+           Nand -> (\p q-> not $ p && q)
+           Xor  ->(\p q->(p || q) && not (p && q)) 
+           Or   -> (||)
+
+simulate st (Emux a1 a2 a3) =
+  case vA1 of
+   VBitArray _ ->
+           VBitArray (zipWith3 (\x y z ->if x then y else z) 
+                      (u vA1) 
+                      (u vA2) 
+                      (u vA3))
+   VBit v1 ->
+           if v1 then vA2 else vA3
+  where vA1 = extractArg st a1
+        vA2 = extractArg st a2
+        vA3 = extractArg st a3
+        u (VBitArray x) = x   -- u for under : through the constructor
 
 --stepSimulation :: Program -> SysState -> SysState
   
