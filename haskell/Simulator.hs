23b2856b18b5202ef8254d4069566548de560a67
thomas bourgeat <thomas.07fr@gmail.com>
2013-11-01 18:34:48 +0100

    Compilation OK, Bad style

--- haskell/Simulator.hs
+++ haskell/Simulator.hs
@@ -10,20 +10,22 @@ import qualified Digraph as G
 
 
 newtype SysState = 
-  SysState{val_vars :: Map.Map ident Value}
+  SysState{val_vars :: Map.Map Ident Value}
 
 extractArg :: SysState -> Arg -> Value
-extractArg st (Avar i) = st Map.! i 
+extractArg st (Avar i) = val_vars st Map.! i 
 extractArg st (Aconst v) = v
 
 
-vLogic:: Binop -> Value -> Value -> Value
+vLogic:: (Bool->Bool->Bool) -> Value -> Value -> Value
 vLogic op v1 v2 =
   case (v1,v2) of
-    (VBit a1,VBit a2)->VBit
-    (VBitArray l1, VBitArray l2)-> VBitArray
-    (VBitArray l1,VBit a1) -> VBitArray
-    (VBit a1,VBitArray l1) -> VBitArray
+    (VBit a1,VBit a2)->VBit (op a1 a2)
+    (VBitArray l1, VBitArray l2)-> VBitArray (map (\(a,b)->op a b) $ zip l1 l2)
+    (VBitArray l1,VBit a1) ->
+         VBitArray (map (\(a,b)->op a b) $ zip l1 (repeat a1))
+    (VBit a1,VBitArray l1) -> 
+         VBitArray (map (\(a,b)->op a b) $ zip l1 (repeat a1))
 
 vNot :: Value -> Value
 vNot v= case v of 
@@ -31,10 +33,8 @@ vNot v= case v of
  (VBitArray l) -> VBitArray (map not $ l) 
 
 
-simulate :: SysState -> Exp -> Value
+--simulate :: SysState -> Exp -> Value
 
 
-
-
-stepSimulation :: Program -> SysState -> SysState
+--stepSimulation :: Program -> SysState -> SysState
   
