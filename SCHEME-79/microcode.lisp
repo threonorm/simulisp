82464fb0a78a04d1a5a0e266102184692a1aaaed
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2013-12-15 16:12:24 +0100

    More info extraction from appendix.

--- SCHEME-79/microcode.lisp
+++ SCHEME-79/microcode.lisp
@@ -107,7 +107,6 @@
 
 ;;;;; 4.1. Variable references
 
-
 (deftype local ; LOCAL<lexical-address>
   (micro-call lookup-exp local-return))
   
@@ -115,5 +114,156 @@
   (assign *val* (&car (fetch *display*)))
   (dispatch-on-stack))
 
+;; Skipping all tail-local stuff
+
+(deftype global
+  (assign *val*
+          (&global-value (fetch *exp*)))
+  (dispatch-on-stack))
+         
+(deftype set-local
+  (micro-call lookup-exp set-local-return))
+
+; &set-global-value = &rplaca
+(deftype set-global
+  (&set-global-value (fetch *exp*) (fetch *val*))
+  (dispatch-on-stack))
+
+(defpc lookup-exp
+  (if (&frame=0?)
+      (progn (assign *display* (&car (fetch *display*)))
+             (go-to-count-displacement))
+      (progn (&decrement-frame)
+             (assign *display* (&cdr (fetch *display*)))
+             (go-to lookup-exp)))
+
+(defpc count-displacement
+  (if (&displacement=0?)
+      (micro-return)
+      (progn (&decrement-displacement)
+             (assign *display* (&cdr (fetch *display*)))
+             (go-to count-displacement))))
+
+;;;;; 4.2. Self-evaluating data
+
+;; multiple types of self-evaluating (immediate|pointer) -> same code
+
+(deftype self-evaluating-immediate
+  (assign *val* (fetch *exp*))
+  (dispatch-on-stack))
+
+(deftype symbol
+  (assign *val* (fetch *exp*))
+  (dispatch-on-stack))
+
+(deftype self-evaluating-pointer
+  (assign *val* (fetch *exp*))
+  (dispatch-on-stack))
+
+;;;;; 4.3. Special operators lambda/if
+
+(deftype procedure
+  (assign *val* (&cons (fetch *exp*) (fetch *display*)))
+  (&set-type val closure)
+  (dispatch-on-stack))
+
+(deftype conditional
+  (if (&eq-val (fetch *nil*))
+      (assign *exp* (&cdr (fetch *exp*)))
+      (assign *exp* (&car (fetch *exp*))))
+  (dispatch-on-exp-allowing-interrupts))
+
+;;;;; 4.4. Compound expressions and sequencing
+
+(defmicromacro save-cdr-and-eval-car (return-tag)
+  (progn (&set-type *retpc-count-mark* ,return-tag)
+         (go-to standard-eval)))
+
+(defpc standard-eval
+  (save (fetch *display*))
+  (&set-type *stack* (fetch *retpc-count-mark*))
+  (save (&cdr (fetch *exp*)))
+  (&set-type *stack* standard-return)
+  (assign *exp* (&car (fetch *exp*)))
+  (dispatch-on-exp-allowing-interrupts))
+
+(defreturn standard-return
+  (restore *exp*)
+  (assign *retpc-count-mark* (fetch *stack*))
+  (restore *display*)
+  (dispatch (fetch *retpc-count-mark*)))
+
+(deftype sequence
+  (assign *val* (fetch *nil*)) ; for gc
+  (save-cdr-and-eval-car sequence-return))
+
+(defreturn sequence-return
+  (dispatch-on-exp-allowin-interrupts))
+
+;;;;; 4.5. Control points (continuations?)
+
+; skipping this...
+
+;;;;; 4.6. Procedure calls
+
+(deftype first-argument
+  (save-cdr-and-eval-car first-argument-return))
+
+(defreturn first-argument-return
+  (assign *args* (&cons (fetch *val*) (fetch *nil*)))
+  (save (fetch *args*))
+  (dispatch-on-exp-allowing-interrupts))
+
+(deftype next-argument
+  (save (fetch *args*))
+  (save-cdr-and-eval-car next-argument-return))
+
+(defreturn next-argument-return
+  (restore *args*)
+  (&rplacd (fetch *args*) (&cons (fetch *val*) (fetch *nil*)))
+  (assign *args* (&cdr (fetch *args*)))
+  (dispatch-on-exp-allowing-interrupts))
+
+(deftype last-argument
+  (save (fetch *args*))
+  (save-cdr-and-eval-car last-argument-return))
+
+(defreturn last-argument-return
+  (restore *args*)
+  (&rplacd (fetch *args*)
+           (&cons (fetch *val*) (fetch *nil*)))
+  (eval-exp-popj-to internal-apply)) ; Amazing! Where did retpc go?
+
+;; special cases for <= 1 arg
+
+(deftype apply-1-arg
+  (save-cdr-and-eval-car apply-1-arg-return))
+
+(defreturn apply-1-arg-return
+  (assign *args* (&cons (fetch *val*) (fetch *nil*)))
+  (save (fetch *args*))
+  (eval-exp-popj-to internal-apply))
+
+(deftype apply-no-args
+  (assign *exp* (&car (fetch *exp*)))
+  (save (fetch *nil*)) ; ugh! need a place for retpc.
+  (eval-exp-popj-to internal-apply))
+
+; skipping spread-argument for now...
+
+
+;;;;; 4.7. Closures (and continuations)
+
+(deftype closure
+  (assign *display*
+          (&cons (fetch *args*) (&cdr (fetch *exp*))))
+  (assign *exp* (&car (&car (fetch *exp*))))
+  (dispatch-on-exp-allowing-interrupts))
+
+; skipping control-point and interrupt-point
+
+;;;;; 4.8. Primitives
+
+; à compléter...  
 
 
