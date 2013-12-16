128bab7d72375d3ed8592c11628d7a71a7525602
thomas bourgeat <thomas.07fr@gmail.com>
2013-12-16 14:05:02 +0100

    Add scode

---
+++ ocaml/scode.ml
@@ -0,0 +1,66 @@
+(* We rely on OCaml's runtime to garbage collect for us*)
+module Gc : 
+  sig
+     type ptr
+     type dataword = 
+        | Nil
+        | Local of int*int
+        | Symb of string
+        | Closure of ptr
+        | Cond of ptr
+        | List of ptr
+        | Num of int
+        | Proc of ptr
+        | Call of ptr
+        | Quote of ptr 
+     type procword = 
+        | More of ptr
+        | Funcall of ptr
+        | Car 
+        | Cdr 
+        | Cons
+        | Incr
+        | Decr
+        | Zero 
+        | Atom
+        | Progn
+        | List
+     val fetch_word : ptr -> dataword
+     val fetch_cell : ptr -> dataword * dataword
+     val fetch_proc_cell : ptr-> dataword*procword
+  end
+    =   
+  struct 
+    type ptr = word * word 
+    type word = 
+        | Data of dataword
+        | Proc of procword
+    and dataword = 
+        | Nil
+        | Local of int*int
+        | Symb of string
+        | Closure of ptr
+        | Cond of ptr
+        | List of ptr
+        | Num of int
+        | Proc of ptr
+        | Call of ptr
+        | Quote of ptr 
+    and procword = 
+        | More of ptr
+        | Funcall of ptr
+        | Car 
+        | Cdr 
+        | Cons 
+        | Atom
+        | Progn
+        | List
+    let fetch_word (Data car,_) = car
+    let fetch_cell (Data car, Data cdr) = (car, cdr) 
+    let fetch_proc_cell (Data car, Proc cdr) = (car,cdr)          
+  end
+
+module Eval =
+  struct
+     
+  end
