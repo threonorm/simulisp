176e570d2770cdf491bf06088210ec83c3a7e984
thomas bourgeat <thomas.07fr@gmail.com>
2014-01-15 22:31:52 +0100

    Refactoring + Example.hs

--- haskell/Assembler/Assembler.hs
+++ haskell/Assembler/Assembler.hs
@@ -8,24 +8,19 @@ import Control.Monad
 import Control.Monad.Fix
 import Data.List
 
-data Reg = Value
-    | Expr
-    | Env
-    | Args
-    | Stack          
-    | Temp
 
-regToString Value = "000"  
-regToString Expr  = "001"
-regToString Env   = "010"
-regToString Args  = "011"
-regToString Stack = "100"
-regToString Temp  = "101"
+--------- MiniAssembler V0.1 -------------
+
 
 data Instruction =
      ExtI ExternalInstruction
     | IntI InternalInstruction
-    | Label (String,Int) --Int for alignment purpose : in number of words 
+    | Label (String,Int)        --Int for alignment purpose : in number of words 
+
+
+--There are three types of instructions : Labels, classical instructions using
+--the mini ALU and finishing by the incrementation of the micro program counter,
+--and jumps (conditional or not).
 
 data ExternalInstruction = 
     C { regRead :: Reg,
@@ -44,15 +39,74 @@ data InternalInstruction =
 
 type Label =  String
 
-ground  = C {regRead = Value,
-           regWrite = Value,
-           writeFlag = False,
-           writeTemp = False,
-           muxData = False,
-           gcOpcode = (False,False),
-           aluCtrl = False,
-           useAlu = False}
 
+data Reg = Value
+    | Expr
+    | Env
+    | Args
+    | Stack          
+    | Temp
+
+type Intermediate =  Either String Instruction 
+
+--MiniAssembler makes two passes
+
+assemble :: [Instruction] -> Maybe String 
+assemble code =
+  if checkPosition inter 0 
+  then Just $ assembleSecond inter 0 inter 
+  else Nothing
+  where inter =  map assembleFirst code
+
+
+assembleFirst :: Instruction -> Intermediate
+assembleFirst (ExtI instr) = Left ("0"++                -- Bit of internal incr 
+ (regToString . regRead $  instr ) ++
+ (regToString . regWrite $  instr ) ++
+ (printBool . writeFlag $  instr ) ++
+ (printBool . writeTemp $  instr ) ++
+ (printBool . muxData $  instr ) ++
+ (printBool . fst . gcOpcode $  instr ) ++
+ (printBool . snd . gcOpcode $ instr ) ++
+ (printBool . aluCtrl $ instr ) ++
+ (printBool . useAlu $ instr ))
+assembleFirst (IntI instr) = 
+  Right (IntI instr) 
+assembleFirst (Label blabla) =
+  Right (Label blabla)
+
+
+
+assembleSecond :: [Intermediate] -> Int -> [Intermediate] -> String  -- Code,position, list from position, label
+assembleSecond code pos [] = []  
+assembleSecond code pos ((Left blabla):q) = "0"++blabla++assembleSecond code (pos+1) q
+assembleSecond code pos ((Right(IntI instr)): q) =  
+ ("1"++ -- Bit of Jump
+  (printBool . isCond $ instr) ++
+  ( printAddr 12 . position code. addr $ instr) ++
+  "0"  --Useless bit in this case
+ ) ++ assembleSecond code (pos+1) q
+assembleSecond code pos ((Right(Label (s,t))): q) = 
+  (take (15*floodSize) $ repeat '0') ++ assembleSecond code t q
+  where floodSize = t-pos 
+
+
+
+--Give the position of the declaration of the Label in the Code (in words).
+position :: [Intermediate] -> Label -> Int
+position ((Right (Label (s,t))):q) lab =
+ if s==lab then t else position q lab
+position (_:q) lab =  position q lab
+
+
+--To check if the assembly is possible with the alignment required by the user.
+checkPosition :: [Intermediate] -> Int -> Bool 
+checkPosition [] prec = True  
+checkPosition (Right(Label (s,t)):q) prec = if prec <= t then checkPosition q t 
+             else False
+checkPosition (_:q) prec = checkPosition q $ prec + 1  --In number of words
+
+---------- MiniASM as a sort of an eDSL  ---------
 
 (^=) :: Reg -> Reg -> Instruction
 reg1 ^= reg2 =  ExtI ground{regRead =  reg2,
@@ -100,57 +154,29 @@ allocCons input output = ExtI $ ground{regRead = input,
                                    gcOpcode = (True,True)
                                   }
 
-type Intermediate =  Either String Instruction 
+----- Administrative tools dedicate to the assembling.-----
+
+regToString Value = "000"  
+regToString Expr  = "001"
+regToString Env   = "010"
+regToString Args  = "011"
+regToString Stack = "100"
+regToString Temp  = "101"
+
+ground  = C {regRead = Value,
+           regWrite = Value,
+           writeFlag = False,
+           writeTemp = False,
+           muxData = False,
+           gcOpcode = (False,False),
+           aluCtrl = False,
+           useAlu = False}
+
+
 
 printBool :: Bool -> String
 printBool True  = "1"  
 printBool False = "0"
-
-
-
-assembleFirst :: Instruction -> Intermediate
-assembleFirst (ExtI instr) = Left ("0"++                                      -- Bit of internal incr 
- (regToString . regRead $  instr ) ++
- (regToString . regWrite $  instr ) ++
- (printBool . writeFlag $  instr ) ++
- (printBool . writeTemp $  instr ) ++
- (printBool . muxData $  instr ) ++
- (printBool . fst . gcOpcode $  instr ) ++
- (printBool . snd . gcOpcode $ instr ) ++
- (printBool . aluCtrl $ instr ) ++
- (printBool . useAlu $ instr ))
- 
-assembleFirst (IntI instr) = 
-  Right (IntI instr) 
-
-
-assembleFirst (Label blabla) =
-  Right (Label blabla)
-
-assembleSecond :: [Intermediate] -> Int -> [Intermediate] -> String  -- Code,position, list from position, label
-assembleSecond code pos [] = []  
-assembleSecond code pos ((Left blabla):q) = "0"++blabla++assembleSecond code (pos+1) q
-assembleSecond code pos ((Right(IntI instr)): q) =  
- ("1"++ -- Bit of Jump
-  (printBool . isCond $ instr) ++
-  ( printAddr 12 . position code. addr $ instr) ++
-  "0"  --Useless bit in this case
- ) ++ assembleSecond code (pos+1) q
-    
-assembleSecond code pos ((Right(Label (s,t))): q) = 
-  (take (15*floodSize) $ repeat '0') ++ assembleSecond code t q
-  where floodSize = t-pos 
-
-position :: [Intermediate] -> Label -> Int
-position ((Right (Label (s,t))):q) lab =
- if s==lab then t else position q lab
-position (_:q) lab =  position q lab
-
-checkPosition :: [Intermediate] -> Int -> Bool
-checkPosition [] prec = True  
-checkPosition (Right(Label (s,t)):q) prec = if prec <= t then checkPosition q t 
-             else False
-checkPosition (_:q) prec = checkPosition q $ prec + 1  --In number of words
 
 
 printAddr :: Int -> Int -> String
@@ -160,23 +186,7 @@ printAddr microAddr pos =
         if pos == 0 then [] else 
           (show $ pos `mod` 2) ++ aux (pos `div` 2)
 
-assemble :: [Instruction] -> Maybe String 
-assemble code =
-  if checkPosition inter 0 
-  then Just $ assembleSecond inter 0 inter 
-  else Nothing
-  where inter =  map assembleFirst code
-
-
-example = [ Label("test",0) ,
-            Value ^= Expr,
-            jmp "test",
-            Label("Hum",22),
-            Expr ^= Stack,
-            condJump "test",
-            fetchCar Value Expr      
-          ]
-            
+           
 
 
  
