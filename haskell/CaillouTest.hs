81a076a5e53763cccc73da1a3f23d136f888d295
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2014-01-15 16:17:38 +0100

    Found a very small example which our netlist generation can't handle.

--- haskell/CaillouTest.hs
+++ haskell/CaillouTest.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE DoRec #-}
+
+import Control.Monad
+import Control.Applicative
+
 import Caillou.Circuit
 import Caillou.Arithmetic
 import Caillou.Patterns
@@ -5,7 +10,15 @@ import Caillou.NetlistGen
 import Netlist.Print
 import Netlist.AST
 
-main = printProgToFile (synthesizeNetlistAST serialAdder (Avar "a", Avar "b") ["a","b"]
-                                             (\s -> [("s", s)]))
-                       "serial-adder.net"
+main = printProgToFile (synthesizeNetlistAST (\() -> test) () []
+                                             (\ss -> zip ["s0", "s1", "s2", "s3"] ss))
+                       "counter.net"
+
+-- test: 4-bit counter
+test = do
+  wireZero <- zero
+  one4bit <- (:) <$> one <*> replicateM 3 zero
+  rec ctr <- mapM delay newctr
+      (newctr,_) <- adder (wireZero, zip one4bit ctr)
+  return ctr
 
