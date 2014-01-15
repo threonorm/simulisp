3ebb211baefc1c553c3cba779331298f1c5cc367
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2014-01-15 22:39:28 +0100

    Displaying integers to the screen!

--- haskell/Simulator/DisplayClock.hs
+++ haskell/Simulator/DisplayClock.hs
@@ -6,12 +6,14 @@ main = SDL.withInit [InitVideo] $ do
          screen <- SDL.setVideoMode 960 400 32 [HWSurface, DoubleBuf]
          SDL.setCaption "Processor demo: Clock" ""
          initial_tick <- SDL.getTicks
-         white <- SDL.mapRGB (SDL.surfaceGetPixelFormat screen) 0   0   0
-         green <- SDL.mapRGB (SDL.surfaceGetPixelFormat screen) 0   255 0
-         execStateT (eventLoop screen initial_tick white green) True
+         let color2px = SDL.mapRGB (SDL.surfaceGetPixelFormat screen)
+         white      <- color2px 0 0   0
+         lightGreen <- color2px 0 255 0
+         darkGreen  <- color2px 0 64  0
+         execStateT (eventLoop screen initial_tick white lightGreen darkGreen) True
          return ()
 
-eventLoop screen initial_tick white green = do
+eventLoop screen initial_tick white lightGreen darkGreen = do
   continueLoop <- get
   when continueLoop $ do
     handleEvents
@@ -19,17 +21,10 @@ eventLoop screen initial_tick white green = do
     let time = (new_tick - initial_tick) `div` 1000
     lift $ do
       SDL.fillRect screen Nothing white
-      draw7seg screen green 20 20
-      draw7seg screen green (20 + width + bigMargin) 20
-      draw7seg screen green (20 + 2*(width + bigMargin)) 20
-      draw7seg screen green (20 + 3*(width + bigMargin)) 20
-      draw7seg screen green (20 + 4*(width + bigMargin)) 20
-      draw7seg screen green (20 + 5*(width + bigMargin)) 20
-      
-      -- SDL.blitSurface text Nothing screen (Just $ Rect 100 100 0 0)
+      draw7segs screen lightGreen darkGreen (12, 42, 07)
       SDL.flip screen
       SDL.delay 50
-    eventLoop screen initial_tick white green
+    eventLoop screen initial_tick white lightGreen darkGreen
 
 handleEvents = do
   e <- lift SDL.pollEvent
@@ -37,24 +32,96 @@ handleEvents = do
     NoEvent -> return ()
     _ -> handleEvent e >> handleEvents
 
+digitTo7Seg 0 = [       True,
+                  True,       True,
+                        False,
+                  True,       True,
+                        True
+                ]
+digitTo7Seg 1 = [       False,
+                  False,      True,
+                        False,
+                  False,      True,
+                        False
+                ]
+digitTo7Seg 2 = [       True,
+                  False,      True,
+                        True,
+                  True,       False,
+                        True
+                ]
+digitTo7Seg 3 = [       True,
+                  False,      True,
+                        True,
+                  False,      True,
+                        True
+                ]
+digitTo7Seg 4 = [       False,
+                  True,       True,
+                        True,
+                  False,      True,
+                        False
+                ]
+digitTo7Seg 5 = [       True,
+                  True,       False,
+                        True,
+                  False,      True,
+                        True
+                ]
+digitTo7Seg 6 = [       True,
+                  True,       False,
+                        True,
+                  True,       True,
+                        True
+                ]
+digitTo7Seg 7 = [       True,
+                  False,      True,
+                        True,
+                  False,      True,
+                        False
+                ]
+digitTo7Seg 8 = [       True,
+                  True,       True,
+                        True,
+                  True,       True,
+                        True
+                ]
+digitTo7Seg 9 = [       True,
+                  True,       True,
+                        True,
+                  False,      True,
+                        True
+                ]
+                
 
-th = 16 -- segment thickness
-lg = 64 -- segment length
-mg = 8 -- margin
-width = 2*th + 2*mg + lg
-bigMargin = 40
-
-draw7seg screen green offsX offsY = do
-  horizontal (offsX + th + mg) (offsY)
-  vertical offsX (offsY + th + mg)
-  vertical (offsX + th + 2*mg + lg) (offsY + th + mg)
-  horizontal (offsX + th + mg) (offsY + th + 2*mg + lg)
-  vertical offsX (offsY + 2*th + 3*mg + lg)
-  vertical (offsX + th + 2*mg + lg) (offsY + 2*th + 3*mg + lg)
-  horizontal (offsX + th + mg) (offsY + 2*th + 4*mg + 2*lg)
-  where horizontal = drawBar lg th
+draw7segs screen lightGreen darkGreen (h,m,s) = do
+  let decompose n = [n `div` 10, n `mod` 10]
+      digits = concatMap decompose [h, m, s]
+  zipWithM (\d x -> drawDigit d x 20)
+           digits
+           [ 20 + k * (width + bigMargin) | k <- [0..5] ]
+  where drawDigit d offsX offsY = do
+          zipWithM_
+            (\switchOn seg -> if switchOn
+                              then seg lightGreen
+                              else seg darkGreen)
+            (digitTo7Seg d)
+            [ horizontal (offsX + th + mg) (offsY)
+            , vertical offsX (offsY + th + mg)                    
+            , vertical (offsX + th + 2*mg + lg) (offsY + th + mg)         
+            , horizontal (offsX + th + mg) (offsY + th + 2*mg + lg)       
+            , vertical offsX (offsY + 2*th + 3*mg + lg)                   
+            , vertical (offsX + th + 2*mg + lg) (offsY + 2*th + 3*mg + lg)
+            , horizontal (offsX + th + mg) (offsY + 2*th + 4*mg + 2*lg)
+            ]
+        horizontal = drawBar lg th
         vertical   = drawBar th lg
-        drawBar w h x y = SDL.fillRect screen (Just $ Rect x y w h) green
+        drawBar w h x y color = SDL.fillRect screen (Just $ Rect x y w h) color
+        th = 16 -- segment thickness
+        lg = 64 -- segment length
+        mg = 0  -- margin
+        width = 2*th + 2*mg + lg
+        bigMargin = 40
 
   
 
