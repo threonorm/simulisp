6c5118c9d6c0e2ad799ef979eca27787443f48b1
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2014-01-17 01:15:48 +0100

    Convert structure of emulator to microprogram.

--- haskell/Processor/Microcode.hs
+++ haskell/Processor/Microcode.hs
@@ -1,76 +1,142 @@
 module Processor.Microcode where
 
+import Control.Arrow
+
 import Assembler.Assembler
 import Processor.Parameters
 import Data.List
 
+blockSize = microAddrS - tagS
 
 selfEvaluating = [ Value ^= Expr,
                    Dispatch Stack ]
 
-blockSize = microAddrS - tagS
-
-processor =  (Label ("Nil",Just 0):selfEvaluating) ++ 
-
-             [Label ("Local",Just blockSize)] ++
-                  walkOnList ++ 
-                  [Dispatch Stack] ++
-
-             [Label ("Global",Just $ 2*blockSize),
-              fetchCar Expr Value,
-              Dispatch Stack]++
-
-             (Label ("Closure",Just $ 3*blockSize): selfEvaluating)++
-
-             [Label("Cond",Just $ 4*blockSize),
-              loadConditional Value,
-              condJump "foobar",
-              fetchCdr Expr Expr,
-              Label("foobar",Nothing),
-              fetchCar Expr Expr ] ++ 
-
-             (Label ("List",Just $ 5*blockSize): selfEvaluating)++
-             
-             (Label ("Num",Just $ 6*blockSize): selfEvaluating)++
-             
-             [Label("Proc",Just $ 7*blockSize),
-              moveToTemp Env,
-              allocConsWithTag Expr Value [True,True,False,False,False],
-             Dispatch Stack
-             ]++  
-             
-              [Label("First",Just $ 8*blockSize)]++
-              saveCdrAndEvalCar returnFirst ++ 
-              
-              [Label("Next",Just $ 9*blockSize),
-               moveToTemp Args,
-               allocCons Stack Stack]++
-               saveCdrAndEvalCar returnNext ++
-              
-              [Label("Last",Just $ 10*blockSize),
-               moveToTemp Args,
-               allocCons Stack Stack]++
-               saveCdrAndEvalCar returnLast ++
-              
-              (Label("ApplyOne",Just $ 11*blockSize):
-               saveCdrAndEvalCar returnApplyOne) ++
-
-              (Label("Let",Just $ 12*blockSize):
-               saveCdrAndEvalCar returnLet) ++              
-               
-              --TODO pritimive
-              (Label("Sequence", Just $ undefined):
-               saveCdrAndEvalCar returnSequence) 
+label x = Label (x, Nothing)
 
 
-              
+eval = [ (TNil     , selfEvaluating)
+       , (TClosure , selfEvaluating)
+       , (TList    , selfEvaluating)
+       , (TNum     , selfEvaluating)
+         
+       , (TLocal   , lookupLocal ++
+                     [Dispatch Stack])
+       , (TGlobal  , [ fetchCar Expr Value
+                     , Dispatch Stack ])
+         
+       , (TCond    , [ loadConditional Value
+                     , condJump "condNotNil"
+                     , fetchCdr Expr Expr
+                     , label "condNotNil"
+                     , fetchCar Expr Expr ])
+         
+       , (TProc    , [ moveToTemp Env
+                     , allocConsWithTag Expr Value (tagBin TClosure)
+                     , Dispatch Stack])
+
+       , (TFirst   , saveCdrAndEvalCar RFirst)
+       , (TNext    , push Args ++
+                     saveCdrAndEvalCar RNext)
+       , (TLast    , push Args ++
+                     saveCdrAndEvalCar RLast)
+         
+       , (TApplyOne, saveCdrAndEvalCar RApplyOne)
+       , (TLet     , saveCdrAndEvalCar RLet)
+       , (TSequence, saveCdrAndEvalCar RSequence)
+       ]
+       ++
+       [ (prim     , selfEvaluating)
+       | prim <- [TCar, TCdr, TCons, TIncr, TDecr, TIsZero,
+                  TIsgt60, TIsgt24, TPrintSec, TPrintMin, TPrintHour] ]
+
+-- kind of a todo list...
+registerNull = undefined
+allocSingleton src dest = [ moveToTemp src
+                          , allocCons registerNull dest ]
+lookupLocal = undefined
+doEval = Dispatch Expr
+doApply = undefined
+getLastArg = fetchCar Args Value
+fetchCarAndIncr = undefined
+fetchCarAndDecr = undefined
+fetchCarAndDecrRetrievingComp = undefined
+fetchCarAndSubImmediate = undefined
+pushWithReturn = undefined
+
+apply = [ (t, allocSingleton Args Env ++
+              [ Expr ^= Value
+              , doEval ])
+        | t <- [TNil, TList, TNum, TLocal, TGlobal, TCond, TProc,
+                TFirst, TNext, TLast, TApplyOne, TLet, TSequence]
+        ]
+        ++
+        map (second (++ [Dispatch Stack]))
+        [ (TCar      , [ getLastArg
+                       , fetchCar Value Value ])
+        , (TCdr      , [ getLastArg
+                       , fetchCdr Value Value ])
+        , (TCons     , [ getLastArg
+                       , moveToTemp Value
+                       , fetchCdr Args Value
+                       , fetchCar Value Value
+                       , allocCons Value Value ])
+        , (TIncr     , [ fetchCarAndIncr Args Value ])
+        , (TDecr     , [ fetchCarAndDecr Args Value ])
+        , (TIsZero   , [ fetchCarAndDecrRetrievingComp Args Value ])
+        , (TIsgt60   , [ fetchCarAndSubImmediate Args Value 60 ])
+        , (TIsgt24   , [ fetchCarAndSubImmediate Args Value 24 ])
+        , (TPrintSec , undefined)
+        , (TPrintMin , undefined)
+        , (TPrintHour, undefined)
+        ]
+
+popToReg reg = [ fetchCar Stack reg
+               , fetchCdr Stack Stack ]
+standardRestore = popToReg Expr ++
+                  popToReg Env
+
+return = [ (RFirst    , standardRestore ++
+                        allocSingleton Value Args ++
+                        [ doEval ])
+           
+         , (RNext     , standardRestore ++
+                        popToReg Args ++
+                        [ moveToTemp Value
+                        , allocCons Args Args
+                        , doEval ])
+           
+         , (RLast     , standardRestore ++
+                        popToReg Args ++
+                        [ moveToTemp Value
+                        , allocCons Args Args ] ++
+                        pushWithReturn Args RApply ++
+                        [ doEval ])
+           
+         , (RApplyOne , standardRestore ++
+                        allocSingleton Value Args ++
+                        pushWithReturn Args RApply ++
+                        [ doEval ])
+           
+         , (RLet      , standardRestore ++
+                        allocSingleton Value Value ++
+                        [ moveToTemp Value
+                        , allocCons Env Env
+                        , doEval ])
+
+         , (RSequence , standardRestore ++
+                        [ doEval ])
+
+         , (RApply    , popToReg Args ++
+                        [ doApply ])
+         ]
+                    
 
 push :: Reg -> [Instruction]                 
 
 push reg = [moveToTemp reg,allocCons Stack Stack]
 
-pushWithReturn :: [Bool] -> Reg -> [Instruction]
-pushWithReturn retTag reg = [moveToTemp reg,allocConsWithTag Stack Stack retTag]
+-- pushWithReturn :: [Bool] -> Reg -> [Instruction]
+-- pushWithReturn retTag reg = [moveToTemp reg,allocConsWithTag Stack Stack retTag]
 
 walkOnList = undefined
 
--- haskell/Processor/Parameters.hs
+++ haskell/Processor/Parameters.hs
@@ -10,5 +10,64 @@ microInstrS = 24
 microAddrS = 12
 -- also: number of registers = 2^3
 
+data Tag = TNil
+         | TLocal
+         | TGlobal
+         | TClosure
+         | TCond
+         | TList
+         | TNum
+         | TProc
+         | TFirst
+         | TNext
+         | TLast
+         | TApplyOne
+         | TLet
+         | TSequence
+         -- Primitives
+         | TCar
+         | TCdr 
+         | TCons
+         | TIncr
+         | TDecr
+         | TIsZero 
+         | TIsgt60
+         | TIsgt24
+         | TPrintSec
+         | TPrintMin
+         | TPrintHour
+
+-- Explicit instead of "deriving Enum" for easy reference
+tagNum :: Tag -> Int
+tagNum TNil      =  0
+tagNum TLocal    =  1
+tagNum TGlobal   =  2
+tagNum TClosure  =  3
+tagNum TCond     =  4
+tagNum TList     =  5
+tagNum TNum      =  6
+tagNum TProc     =  7
+tagNum TFirst    =  8
+tagNum TNext     =  9
+tagNum TLast     = 10
+tagNum TApplyOne = 11
+tagNum TLet      = 12
+tagNum TSequence = 13
+
+tagBin :: Tag -> [Bool]
+tagBin = go tagS . tagNum
+  where go 0 _ = []
+        go k n = (n `mod` 2 /= 0) : go (k-1) (n `div` 2)
+
+
+data ReturnTag = RFirst
+               | RNext
+               | RLast
+               | RApplyOne           
+               | RLet
+               | RSequence
+               | RApply
+                 
+
 -- microinstruction format: see Hardware.hs
 
